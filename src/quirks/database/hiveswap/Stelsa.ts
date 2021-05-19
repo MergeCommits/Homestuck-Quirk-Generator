5cdd1048ce2e9f135d25bec217e4a0a52266c2d0
QuaternionMark <markjarjour@gmail.com>
2021-05-19 16:35:08 -0400

    Fixed Stelsa's name.

--- src/quirks/database/hiveswap/Stelsa.ts
+++ src/quirks/database/hiveswap/Stelsa.ts
@@ -2,7 +2,7 @@ import Quirk from "quirks/Quirk";
 
 export default class Stelsa extends Quirk {
     public constructor() {
-        super("Stelsa Lamati");
+        super("Stelsa Sezyat");
     }
 
     protected quirkify(): void {
