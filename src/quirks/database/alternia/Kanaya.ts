f89496ab87b0c65be12672ee97b927328276e6ae
QuaternionMark <markjarjour@gmail.com>
2021-07-20 22:30:48 -0400

    Cleaned up Kanaya's code and added contraction mark remover.

--- src/quirks/database/alternia/Kanaya.ts
+++ src/quirks/database/alternia/Kanaya.ts
@@ -6,26 +6,10 @@ export default class Kanaya extends Quirk {
     }
 
     protected quirkify(): void {
-        // Any letter with whitespace preceding.
-        let cap1 = "\\s";
-        // Any letter at the start of the string.
-        let cap2 = "^";
-        // Any letter preceded by punctuation (except ' and `).
-        let cap3 = "[.,!?\\/\\|]";
+        const contractionMarks = "(\\b)['`,](\\b)";
+        this.replaceString(contractionMarks, "$1$2");
 
-        // Any of the above succeeded by a ' or `.
-        // All of this is to distinguish between a word wrapped in single quotes and a contraction.
-        let cap4 = cap1 + "['`]";
-        let cap5 = cap2 + "['`]";
-        let cap6 = cap3 + "['`]";
-
-        cap1 += "\\w";
-        cap2 += "\\w";
-        cap3 += "\\w";
-        cap4 += "\\w";
-        cap5 += "\\w";
-        cap6 += "\\w";
-
-        this.upperCase(`(${cap1}|${cap2}|${cap3}|${cap4}|${cap5}|${cap6})`);
+        const firstLetterOfWords = "\\b\\w";
+        this.upperCase(firstLetterOfWords);
     }
 }
